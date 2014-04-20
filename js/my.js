228b0748e4b67c590b8cfc654bd0fe3779606e3d
titovmaxim <m.m.titov@gmail.com>
2014-04-20 23:00:40 +0800

    zxcv

--- js/my.js
+++ js/my.js
@@ -2,9 +2,9 @@
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
-		var t = $anchor.attr('href');
+		var $t = $($anchor.attr('href'));
         $('html, body').stop().animate({
-            scrollTop: $(t).offset().top
+            scrollTop: $t.offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });

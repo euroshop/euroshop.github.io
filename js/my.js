cd8b7a4fc87a7aef89986ab1ecb667b7e3060b04
titovmaxim <m.m.titov@gmail.com>
2014-04-20 23:46:27 +0800

    update scroll

--- js/my.js
+++ js/my.js
@@ -1,12 +1,15 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-		var name = $anchor.attr('href').substr(1);
-		var target = $('a[name=' + name + ']');
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000, 'easeInOutSine');
-        event.preventDefault();
-    });
+	$(".nav a[href^='#']").bind('click', function(event) {
+		var hash = this.hash;
+
+		$('html, body').stop().animate(
+			{scrollTop: $(hash).offset().top},
+			1000,
+			'easeInOutSine',
+			function() { window.location.hash = hash; }
+		);
+
+		event.preventDefault();
+	});
 });

c1705a40849cc2fdcbd650bd24065136b8004d7f
titovmaxim <m.m.titov@gmail.com>
2014-04-21 10:51:34 +0800

    ads

--- js/my.js
+++ js/my.js
@@ -3,15 +3,15 @@ $(function() {
 	$('body').scrollspy({ target: '.inner_page_nav' })
 
 	$("a[href^='#']").bind('click', function(event) {
-		return;
 		var hash = $(this).attr('href');
 		var top = $(hash.length > 1 ? hash : 'body').offset().top;
 
+		window.location.hash = hash;
 		$('html, body').stop().animate(
 			{ scrollTop: top },
 			1000,
 			'easeInOutCubic',
-			function() { window.location.hash = hash; }
+//			function() { window.location.hash = hash; }
 		);
 
 		event.preventDefault();

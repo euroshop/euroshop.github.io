51db08af00957e95b630c37c40c89aa7c564cd3c
titovmaxim <m.m.titov@gmail.com>
2014-04-21 11:11:28 +0800

    asdasdasd

--- js/my.js
+++ js/my.js
@@ -3,13 +3,7 @@ $(function() {
 	$('body').scrollspy({ target: '.inner_page_nav' })
 
 	$("a[href^='#']").bind('click', function(event) {
-		if(!history.pushState) {
-			reutrn;
-		}
-		
 		var hash = $(this).attr('href');
-		history.pushState(null, null, hash);
-		
 		var top = $(hash.length > 1 ? hash : 'body').offset().top;
 		$('html, body').stop().animate(
 			{ scrollTop: top },

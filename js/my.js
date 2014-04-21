8d291215b56e728306ccd5d5a40bd0747eaa4b67
titovmaxim <m.m.titov@gmail.com>
2014-04-21 11:04:19 +0800

    asdasd

--- js/my.js
+++ js/my.js
@@ -3,17 +3,19 @@ $(function() {
 	$('body').scrollspy({ target: '.inner_page_nav' })
 
 	$("a[href^='#']").bind('click', function(event) {
+		if(!history.pushState) {
+			reutrn;
+		}
+		
 		var hash = $(this).attr('href');
+		history.pushState(null, null, hash);
+		
 		var top = $(hash.length > 1 ? hash : 'body').offset().top;
-
-		window.location.hash = hash;
 		$('html, body').stop().animate(
-			{ scrollTop: top-100 },
+			{ scrollTop: top },
 			1000,
 			'easeInOutCubic'
-//			function() { window.location.hash = hash; }
 		);
-
 		event.preventDefault();
 	});
 });

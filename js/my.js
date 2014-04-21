d9a2a25aaf53bbe8c04a1edc75170c62a1272e39
titovmaxim <m.m.titov@gmail.com>
2014-04-21 10:30:57 +0800

    zczasd

--- js/my.js
+++ js/my.js
@@ -3,13 +3,17 @@ $(function() {
 	$('body').scrollspy({ target: '.inner_page_nav' })
 
 	$("a[href^='#']").bind('click', function(event) {
-		var top = $(this.hash.length <= 1 ? 'body' : this.hash).offset().top;
+		var hash = $(this).attr('href');
+		var top = $(hash.length <= 1 ? 'body' : hash).offset().top;
 
 		$('html, body').stop().animate(
 			{ scrollTop: top },
 			1000,
 			'easeInOutCubic'
 		);
+
+		//function() { window.location.hash = hash; }
+		window.location.hash = hash;
 
 		event.preventDefault();
 	});

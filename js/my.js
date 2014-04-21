a4ce91256ad22ed534734db611ce50fef7898f97
titovmaxim <m.m.titov@gmail.com>
2014-04-22 03:47:45 +0800

    site 1c

--- js/my.js
+++ js/my.js
@@ -1,10 +1,10 @@
 // scrolling and spying
 $(function() {
-	$('body').scrollspy({ target: '.inner_page_nav' })
+	$('body').scrollspy({ target: '.inner_page_nav', offset: 200 })
 
 	$("a[href^='#']").bind('click', function(event) {
 		var hash = $(this).attr('href');
-		var top = $(hash.length > 1 ? hash : 'body').offset().top;
+		var top = $(hash.length > 1 ? hash : 'body').offset().top - $('body').css('padding-top').replace('px', '');
 		$('html, body').stop().animate(
 			{ scrollTop: top },
 			1000,
